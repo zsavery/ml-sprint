495b2bf0ccd84a77c5dd3300428825d69d064adf
zsavery <zsavery998@gmail.com>
2025-09-06 15:46:15 -0500

    trees

--- tree.py
+++ tree.py
@@ -2,14 +2,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn import metrics
 
-np.random.seed(42)
+if __name__ == '__main__':
 
-actual = np.random.binomial(1, 0.9, 1000)
-predicted = np.random.binomial(1, 0.9, 1000)
+    np.random.seed(42)
 
-conf_matrix = metrics.confusion_matrix(actual, predicted)
+    actual = np.random.binomial(1, 0.9, 1000)
+    predicted = np.random.binomial(1, 0.9, 1000)
 
-conf_display = metrics.ConfusionMatrixDisplay(confusion_matrix=conf_matrix,
-                                              display_labels=[True, False])
-conf_display.plot()
-plt.show()
+    conf_matrix = metrics.confusion_matrix(actual, predicted)
+
+    conf_display = metrics.ConfusionMatrixDisplay(confusion_matrix=conf_matrix,
+                                                  display_labels=[True, False])
+    conf_display.plot()
+    plt.show()
